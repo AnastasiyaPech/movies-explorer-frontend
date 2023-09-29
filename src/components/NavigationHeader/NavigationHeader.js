8645561b94267f063e215e5a8e7b7c384c4a4180
Anastasiya Pechnikova <anastasiyaverty@yandex.ru>
2023-09-29 12:51:21 +0300

    added burger-menu

--- src/components/NavigationHeader/NavigationHeader.js
+++ src/components/NavigationHeader/NavigationHeader.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 
 function NavigationHeader() {
-    const location = useLocation().pathname;
-    return (
-        <nav className="navigation__links">
-            <Link to="/movies" className="navigation__link">Фильмы</Link>
-            <Link to="/saved-movies" className="navigation__link">Сохранённые фильмы</Link>
-            <Link to="/profile" className="navigation__link navigation__link_container">
-                <p className="navigation__text">Аккаунт</p>
-                <button type="button" className={`navigation__button ${location ==="/" && "navigation__button_pink"}`}></button>
-            </Link>
-        </nav>
 
+    const location = useLocation().pathname;
+    // бургер меню
+    const [isOpen, setOpen] = useState();
+    function handleSetNavClick() {
+        setOpen(!isOpen);
+    }
+
+    return (
+        <>
+            < nav className= {`navigation__links ${isOpen ? "active" : ""}`}>
+                <Link to="/movies" className="navigation__link navigation__link_main">Главная</Link>
+                <Link to="/movies" className="navigation__link">Фильмы</Link>
+                <Link to="/saved-movies" className="navigation__link">Сохранённые фильмы</Link>
+                <Link to="/profile" className="navigation__link navigation__link_container">
+                    <p className="navigation__text">Аккаунт</p>
+                    <button type="button" className={`navigation__button ${location === "/" && "navigation__button_pink"}`}></button>
+                </Link>
+            </nav>
+            <div className="burgermenu__container">
+                <button type="button"
+                    className={`button__burger-menu ${isOpen ? "button__burger-menu_close" : "button__burger-menu"}`}
+                    onClick={handleSetNavClick}
+                >
+                </button>
+            </div>
+        </>
     );
 }
 
